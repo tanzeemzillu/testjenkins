09902e7a6fc66bceed27d3733ef116922414a616
Tanzeem <tanzeemmd.zillu@realdigital.de>
2018-03-19 15:20:49 +0100

    My code

---
+++ Hello/src/de/real/Hello.java
@@ -0,0 +1,18 @@
+package de.real;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<10; i++) {
+			
+			System.out.println("Hello......" +i);
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+
+	}
+
+}
